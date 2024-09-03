4258491ddec10462b2142b443d2e456ce0a63385
siaochi <chichigou.work@gmail.com>
2024-09-03 18:52:41 +0800

    Add: fade-in effect in index.css

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,21 +4,22 @@ import logo from '/logo.png';
 
 function Menu() {
   return (
-    <header className="text-white flex items-center justify-between p-4 bg-bg-yellow">
+    <header className="fixed top-0 left-0 w-full text-white flex items-center justify-between p-4 bg-bg-yellow z-50">
       {/* Logo */}
       <div className="flex-shrink-0">
         <img src={logo} alt="Logo" className="w-[80%]" /> 
       </div>
       {/* Navigation Menu */}
       <nav className="flex space-x-12 px-14">
-        <a href="/" className="text-black hover:bg-c-orange rounded-full p-2   font-serif text-lg font-bold tracking-wider">跨年出國抽</a>
-        <a href="/#first" className="text-black hover:bg-c-orange rounded-full p-2   font-serif text-lg font-bold tracking-wider">滿保滿額抽</a>
-        <a href="/#second" className="text-black hover:bg-c-orange rounded-full p-2   font-serif text-lg font-bold tracking-wider">首次投保抽</a>
-        <a href="/#third" className="text-black hover:bg-c-orange rounded-full p-2   font-serif text-lg font-bold tracking-wider">分享全球抽</a>
-        <a href="/policy" className="text-black hover:bg-c-orange rounded-full p-2  font-serif text-lg font-bold tracking-wider">活動辦法</a>
+        <a href="/" className="text-black hover:bg-c-orange rounded-full p-2 font-serif text-lg font-bold tracking-wider">跨年出國抽</a>
+        <a href="/#first" className="text-black hover:bg-c-orange rounded-full p-2 font-serif text-lg font-bold tracking-wider">滿保滿額抽</a>
+        <a href="/#second" className="text-black hover:bg-c-orange rounded-full p-2 font-serif text-lg font-bold tracking-wider">首次投保抽</a>
+        <a href="/#third" className="text-black hover:bg-c-orange rounded-full p-2 font-serif text-lg font-bold tracking-wider">分享全球抽</a>
+        <a href="/policy" className="text-black hover:bg-c-orange rounded-full p-2 font-serif text-lg font-bold tracking-wider">活動辦法</a>
       </nav>
     </header>
   );
 }
+
 
 export default Menu;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,20 +3,26 @@ import Menu from "../../components/Menu/Menu";
 
 function HomePage() {
   return (
+    
     <div className="relative bg-bg-yellow">
-        <img src="/home/Polygon.svg" className="absolute top-[23%] " alt="background triangle"></img>
+        {/* 背景圖像 */}
+        <img src="/home/Polygon.svg" className="absolute top-[23%]" alt="background triangle"></img>
+    
+    {/* 固定在頂部的 Menu */}
     <Menu />  
-     <section className=" bg-bg-yellow flex items-center justify-center" >
+
+    {/* 主內容區域，設定 margin-top 以避開 Menu */}
+     <section className=" bg-bg-yellow flex items-center justify-center pt-[78px]" >
         <div className="flex w-screen">
             <div className="relative flex-[4] h-[calc(100vh-78px)] w-full" >
                 <div className="flex flex-col items-center w-full h-full top-[23%] absolute left-[10%] bottom-[10%]">
-                    <img className='w-[33vw]' src='/home/event_title1.svg'></img>
+                    <img className='w-[33vw] fade-in-from-left' src='/home/event_title1.svg'></img>
                     <div className="my-[5%]">
-                        <p className="text-center font-bold tracking-widest text-[1.3vw]">保全球旅平險</p>
-                        <p className="text-center font-bold tracking-widest text-[1.3vw]">驚喜<span className="text-c-blue">多多多多多</span>到滿出來 !</p>
+                        <p className="text-center font-bold tracking-widest text-[1.3vw] fade-in-from-left">保全球旅平險</p>
+                        <p className="text-center font-bold tracking-widest text-[1.3vw] fade-in-from-left">驚喜<span className="text-c-blue">多多多多多</span>到滿出來 !</p>
                     </div>
                 </div>
-                <div className="absolute bottom-[2%] left-[5%] w-full">
+                <div className="absolute bottom-[2%] left-[5%] w-full fade-in-from-left">
                     <p className="leading-none  text-c-blue font-extrabold text-[4vw] whitespace-nowrap tracking-wider">TRAVEL</p>
                     <p className="leading-none text-c-blue font-extrabold text-[4vw] whitespace-nowrap tracking-wider">A TRANSGLOBE JOURNEY</p>
                 </div>
@@ -24,13 +30,13 @@ function HomePage() {
 
             <div className="flex-[6]  w-full">
                 <div className="relative h-[calc(100vh-90px)]">
-                        <div className="absolute w-full h-full top-[2%] right-[3%]" style={{ backgroundImage: "url('/home/flower.png')" }}></div>
-                        <img src='/home/luggage.png' className="w-[60%] absolute bottom-[10%] left-[15%]"></img>
-                        <img src='/home/flight.png' className="w-[43%] absolute bottom-[48%] left-[25%]"></img>
-                        <img src='/home/coupon.png' className="w-[16%] absolute bottom-[75%] left-[2%]"></img>
-                        <img src='/home/god.png' className="w-[20%] absolute bottom-[31%] left-[62%]"></img>
-                        <img src='/home/tower.png' className="w-[18%] absolute bottom-[38%] left-[16%]"></img>
-                        <img src='/home/camera.png' className="w-[13%] absolute bottom-[20%] left-[2%] "></img>
+                        <div className="absolute w-full h-full top-[2%] right-[3%] fade-in-from-top" style={{ backgroundImage: "url('/home/flower.png')", animationDelay: '300ms' }}></div>
+                        <img src='/home/luggage.png' className="w-[60%] absolute bottom-[10%] left-[15%] fade-in-from-bottom "  ></img>
+                        <img src='/home/god.png' className="w-[20%] absolute bottom-[31%] left-[62%] fade-in-from-bottom" style={{ animationDelay: '700ms' }}></img>
+                        <img src='/home/tower.png' className="w-[18%] absolute bottom-[38%] left-[16%] fade-in-from-bottom" style={{ animationDelay: '700ms' }}></img>
+                        <img src='/home/flight.png' className="w-[43%] absolute bottom-[48%] left-[25%] fade-in-from-right" ></img>
+                        <img src='/home/coupon.png' className="w-[16%] absolute bottom-[75%] left-[2%] fade-in-from-bottom" ></img>
+                        <img src='/home/camera.png' className="w-[13%] absolute bottom-[20%] left-[2%] fade-in-from-bottom"  style={{ animationDelay: '300ms' }}></img>
                 </div>
             </div>
         </div>
@@ -43,7 +49,7 @@ function HomePage() {
         </div>
       </section>
 
-      <section className="h-screen  flex items-center justify-center">
+      <section className="h-screen  flex items-center justify-center ">
         <div>
           <h2 className="text-2xl font-semibold text-gray-800 mb-2">About Us</h2>
           <p className="text-gray-700">Provide a brief introduction about your website or company here.</p>
