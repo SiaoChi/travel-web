a6e14488d2c5c5e25f81f349141b2646ef4342be
Kelly Guo <98171354+SiaoChi@users.noreply.github.com>
2024-09-05 15:08:08 +0800

    Container (#13)
    * add: tailwind container to handle RWD
    * modify: menu text to pixel
    * modify: KV paper flower position to hero-section

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,23 +4,23 @@ import logo from '/logo.png';
 
 function Menu() {
   return (
-    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-screen w-min-[1280px] text-white flex items-center justify-between p-4 bg-bg-yellow z-50">
+    <header className=" fixed top-0 left-1/2 transform -translate-x-1/2 w-[98vw] w-min-[1280px] text-white flex items-center justify-between p-4 bg-bg-yellow z-50">
 
-      <div className="flex-shrink-0 ml-[1vw]">
+      <div className="flex-shrink-0 ml-[2vw]">
         <a href="https://www.transglobe.com.tw/" target="_blank" rel="noopener noreferrer">
           <img src={logo} alt="Logo" className="" />
         </a>
       </div>
       {/* Navigation Menu */}
       <nav className="flex space-x-7 px-14">
-        <a href="./#sec1" className="relative text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[1.4vw] font-bold tracking-wider">
+        <a href="./#sec1" className="relative text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[22px] font-bold tracking-wider">
             跨年出國抽
-            <img src="./menu/new.svg" alt="New" className="absolute top-[-1%] right-[-1%] w-[2vw] h-[1vw]" />
+            <img src="./menu/new.svg" alt="New" className="absolute top-[-10%] right-[-1%] w-[30px] h-[30px]" />
         </a>        
-        <a href="./#sec2" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[1.4vw] font-bold tracking-wider">滿保滿額抽</a>
-        <a href="./#sec3" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[1.4vw] font-bold tracking-wider">首次投保抽</a>
-        <a href="/mgm" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[1.4vw] font-bold tracking-wider">分享全球抽</a>
-        <a href="/policy" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[1.4vw] font-bold tracking-wider">活動辦法</a>
+        <a href="./#sec2" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[22px] font-bold tracking-wider">滿保滿額抽</a>
+        <a href="./#sec3" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[22px] font-bold tracking-wider">首次投保抽</a>
+        <a href="/mgm" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[22px] font-bold tracking-wider">分享全球抽</a>
+        <a href="/policy" className="text-black hover:bg-c-orange rounded-full py-2 px-4 font-serif text-[22px] font-bold tracking-wider">活動辦法</a>
       </nav>
     </header>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,43 +3,51 @@
 function HomePage() {
   return (
     
-    <div className="bg-bg-yellow">
-      <div className="relative bg-bg-yellow mx-auto">
-          {/* 背景圖像 */}
-          <img src="./home/Polygon.svg" className="absolute top-[13%] z-0" alt="background triangle"></img>
-          <img src='./home/shadow.png' className="w-[70%] absolute top-[10%] right-[3%]  fade-in-from-bottom" ></img>
-          <div className="absolute w-[60vw] h-[560px] top-[3%] right-[1%] fade-in-from-top z-0" style={{ backgroundImage: "url('./home/flower.png')", animationDelay: '800ms' }}></div>
+    <div className="bg-bg-yellow relative">
+      
+          {/* 背景三角形 */}
+          <img src="./home/Polygon.svg" className="w-[35vw] absolute top-[13%] z-0" alt="background triangle"></img>
+                  
+
+      <div className=" bg-bg-yellow  mx-auto container">
     
-      {/* KV 有設定 padding-top 以避開 Menu */}
-        <section className="bg-bg-yellow flex items-center justify-center pt-[10vh] w-full h-[720px] mx-auto" >
-            <div className="flex w-full max-w-screen-xl">
-              {/* 左側區塊 */}
-                <div className="relative flex-[4]" >
-                    <div className="flex flex-col items-center w-full absolute top-[26%] left-[8%]">
-                        <img className='w-full fade-in-from-left' src='./home/event_title1.svg'></img>
-                        <div className="my-[20px]">
-                            <p className="text-center font-bold tracking-[0.6vw] text-[25px] fade-in-from-left\">保全球旅平險</p>
-                            <p className="text-center font-bold tracking-[0.6vw] text-[25px] fade-in-from-left">驚喜<span className="text-c-blue">多多多多多</span>到滿出來 !</p>
+      {/* Hero-section 有設定 padding-top 以避開 Menu */}
+        <section className="flex-col md:flex items-center justify-center pt-[10vh] container lg:h-[720px] mx-auto" >
+            <div className="relative flex w-full max-w-screen-xl ">
+
+             {/* 背景紙花 因跟著主視覺需要對位清楚 */}
+            <div className="flex absolute w-[59%] h-[560px] right-[6%] top-[8%] fade-in-from-top z-0" style={{ backgroundImage: "url('./home/flower.png')", animationDelay: '800ms' }}></div>
+            
+            {/* 左下角固定英文字 */}
+            
+              <div className="absolute left-[3vw]  md:left-[1vw] bottom-[2vw] w-full fade-in-from-left z-10 " style={{ animationDelay: '400ms' }}>
+                  <p className="leading-none  text-c-blue font-extrabold text-[30px] md:text-[40px] lg:text-[60px] whitespace-nowrap tracking-wider">TRAVEL</p>
+                  <p className="leading-none text-c-blue font-extrabold text-[30px]  md:text-[40px] lg:text-[60px] whitespace-nowrap tracking-wider">A TRANSGLOBE JOURNEY</p>
+              </div>
+    
+                  {/* Hero-section 左側 */}
+                    <div className="relative flex-[4] ">
+                        <div className="flex flex-col items-center w-full absolute md:top-[26%] left-[8%]">
+                            <img className='w-full fade-in-from-left' src='./home/event_title1.svg'></img>
+                            <div className="my-[20px]">
+                                <p className="text-center font-bold tracking-[0.6vw] text-[16px]  md:text-[25px] fade-in-from-left\">保全球旅平險</p>
+                                <p className="text-center font-bold tracking-[0.6vw] text-[16px]  md:text-[25px] fade-in-from-left">驚喜<span className="text-c-blue">多多多多多</span>到滿出來 !</p>
+                            </div>
                         </div>
                     </div>
-                    {/* 左下角固定英文字 */}
-                    <div className="absolute left-[1%] bottom-[4%] w-full fade-in-from-left z-10 " style={{ animationDelay: '400ms' }}>
-                        <p className="leading-none  text-c-blue font-extrabold text-[60px] whitespace-nowrap tracking-wider">TRAVEL</p>
-                        <p className="leading-none text-c-blue font-extrabold text-[60px] whitespace-nowrap tracking-wider">A TRANSGLOBE JOURNEY</p>
-                    </div>
-                </div>
 
-              {/* 右側區塊 */}
-                <div className="flex-[6] w-[760px]">
-                    <div className="kv-right relative h-[720px]">
-                            <img src='./home/luggage.png' className="w-[63%] absolute bottom-[12%] left-[18%] fade-in-from-bottom"  ></img>
-                            <img src='./home/god.png' className="w-[22%] absolute bottom-[29%] left-[67%] fade-in-from-bottom" style={{ animationDelay: '500ms' }}></img>
-                            <img src='./home/tower.png' className="w-[21%] absolute bottom-[39%] left-[19%] fade-in-from-bottom" style={{ animationDelay: '500ms' }}></img>
-                            <img src='./home/flight.png' className="w-[45%] absolute bottom-[46%] left-[27%] fade-in-from-right"style={{ animationDelay: '300ms' }} ></img>
-                            <img src='./home/coupon.png' className="w-[16%] absolute bottom-[75%] left-[4%] fade-in-from-bottom" ></img>
-                            <img src='./home/camera.png' className="w-[14%] absolute bottom-[18%] left-[5%] fade-in-from-bottom" ></img>
+                  {/* Hero-section 右側 */}
+                    <div className="flex-[6] md:w-[800px] ">
+                        <img src='./home/shadow.png' className="absolute bottom-[14%] left-[25%]  fade-in-from-bottom" ></img>
+                        <div className="kv-right relative h-[300px] md:h-[430px] lg:h-[720px]">
+                                <img src='./home/luggage.png' className="w-[63%] absolute bottom-[12%] left-[18%] fade-in-from-bottom"  ></img>
+                                <img src='./home/god.png' className="w-[22%] absolute bottom-[29%] left-[67%] fade-in-from-bottom" style={{ animationDelay: '500ms' }}></img>
+                                <img src='./home/tower.png' className="w-[21%] absolute bottom-[39%] left-[19%] fade-in-from-bottom" style={{ animationDelay: '500ms' }}></img>
+                                <img src='./home/flight.png' className="w-[45%] absolute bottom-[46%] left-[27%] fade-in-from-right"style={{ animationDelay: '300ms' }} ></img>
+                                <img src='./home/coupon.png' className="w-[16%] absolute bottom-[75%] left-[4%] fade-in-from-bottom" ></img>
+                                <img src='./home/camera.png' className="w-[14%] absolute bottom-[18%] left-[5%] fade-in-from-bottom" ></img>
+                        </div>
                     </div>
-                </div>
             </div>
           </section>
 
@@ -69,13 +77,13 @@ function HomePage() {
             </div>
           </section>
 
-          {/* LINE POINT區塊 */}
-            <section className="h-[720px]  flex items-center justify-center" >
-            <div>
-              <h2 className="text-2xl font-semibold text-gray-800 mb-2">Final Section</h2>
-              <p className="text-gray-700">Provide a brief introduction about your website or company here.</p>
-            </div>
-          </section>
+        {/* LINE POINT區塊 */}
+          <section className="h-[720px]  flex items-center justify-center" >
+          <div>
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">Final Section</h2>
+            <p className="text-gray-700">Provide a brief introduction about your website or company here.</p>
+          </div>
+        </section>
     </div>
    </div>
   );
