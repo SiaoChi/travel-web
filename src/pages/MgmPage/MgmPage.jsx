d1b4d2a03592982f44c420fb9c83a87ef5cb04ac
Mac <tzuhualiao@gmail.com>
2024-09-10 22:01:01 +0800

    add mgm banner content

--- src/pages/MgmPage/MgmPage.jsx
+++ src/pages/MgmPage/MgmPage.jsx
@@ -19,9 +19,48 @@ const Container = styled.div`
 
 const YellowBackground = styled.img`
   position: absolute;
-  top: 417px;
+  top: 507px;
   left: 0;
   width: 100%;
+`
+
+const BannerFlower = styled.img`
+  position: absolute;
+  top: 0;
+  right: 20px;
+  width: 964px;
+`
+
+
+const BannerTitle = styled.img`
+    position: absolute;
+    top: 100px;
+    left: 100px;
+    width: 500px;
+`
+
+const BannerTitlePoint = styled.img`
+    position: absolute;
+    top: 100px;
+    left: 100px;
+    width: 500px;
+`
+
+const Desc = styled.div`
+    position: absolute;
+    top: 380px;
+    left: 120px;
+    width: 456px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    line-height: 35px;
+    letter-spacing: 4px;
+    font-size: 25px;
+    font-weight: bold;
+    > p > span {
+        color: #408CC9;
+    }
 `
 
 const MgmPage = () => {
@@ -29,9 +68,15 @@ const MgmPage = () => {
         <Wrap>
             <Container>
                 <YellowBackground src="./mgm/mgm-trapezoid.svg" />
+                <BannerFlower src="./mgm/mgm-banner-flower.svg" />
                 
-          
-  
+                <BannerTitle src="./mgm/mgm-title.png" />
+                <BannerTitlePoint src="./mgm/mgm-title-p.png" />
+                <Desc>
+                    <p>推薦好友全球旅平險</p>
+                    <p>快樂<span>+1+1+1+1+1+1+1+1+1</span></p>
+                </Desc>
+        
             </Container>
       </Wrap>
     )
