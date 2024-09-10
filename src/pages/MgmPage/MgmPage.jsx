50239f467113d3990a417eff87cdb1b57ff62887
Mac <tzuhualiao@gmail.com>
2024-09-10 21:50:13 +0800

    add yellow background

--- src/pages/MgmPage/MgmPage.jsx
+++ src/pages/MgmPage/MgmPage.jsx
@@ -1,10 +1,39 @@
+import styled from "styled-components";
 
+const Wrap = styled.div`
+  padding-top: 90px;
+  background-color: #FFFAF1;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  font-family: Noto Sans TC;
+
+`
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1500px;
+  position: relative;
+  height: 2645px;
+`
+
+const YellowBackground = styled.img`
+  position: absolute;
+  top: 417px;
+  left: 0;
+  width: 100%;
+`
 
 const MgmPage = () => {
     return (
-        <div>
-            <h1>MGM</h1>
-        </div>
+        <Wrap>
+            <Container>
+                <YellowBackground src="./mgm/mgm-trapezoid.svg" />
+                
+          
+  
+            </Container>
+      </Wrap>
     )
 }
 
