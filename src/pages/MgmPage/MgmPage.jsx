15d8a4bce571c3d9742e2ce9686092eff3e5bf01
Mac <tzuhualiao@gmail.com>
2024-09-10 23:08:53 +0800

    add mgm main content

--- src/pages/MgmPage/MgmPage.jsx
+++ src/pages/MgmPage/MgmPage.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
   width: 100%;
   max-width: 1500px;
   position: relative;
-  height: 2645px;
+  height: 2780px;
 `
 
 const YellowBackground = styled.img`
@@ -33,21 +33,21 @@ const ContentFlower = styled.img`
   position: absolute;
   top: 700px;
   left: 60px;
-  width: 1232px;
+  width: 1300px;
 `
 
 const ContentPoint = styled.img`
   position: absolute;
   top: 660px;
   left: 78px;
-  width: 1260px;
+  width: 1360px;
 `
 
 const BackgroundBuilding = styled.img`
   position: absolute;
   bottom: 375px;
   left: 100px;
-  width: 1212px;
+  width: 1312px;
 `
 
 const BlueBackground = styled.img`
@@ -99,6 +99,80 @@ const EnglishTitle = styled.p`
     color: #2496F0;
 `
 
+const ContentDesc = styled(Desc)`
+    font-size: 20px;
+    top: 730px;
+    left: 50%;
+    transform: translateX(-50%);
+`
+
+const LinePointsText = styled.img`
+    position: absolute;
+    top: 790px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 387px;
+`
+
+const MgmContentOne = styled.img`
+    position: absolute;
+    top: 900px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 974px;
+`
+
+const MgmContentTwo = styled.img`
+    position: absolute;
+    top: 1163px;
+    left: 50%;
+    transform: translateX(-48%);
+    width: 1010px;
+`
+
+const MgmContentThree = styled.img`
+    position: absolute;
+    top: 1463px;
+    left: 50%;
+    transform: translateX(-52%);
+    width: 928px;
+`
+
+const People = styled.img`
+    position: absolute;
+    top: 1800px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 821px;
+`
+
+const  ActionDesc = styled(Desc)`
+    font-size: 20px;
+    top: 2110px;
+    left: 50%;
+    transform: translateX(-50%);
+    letter-spacing: 1px;
+`
+
+const ActionLinePoints = styled.img`
+    position: absolute;
+    top: 2160px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 717px;
+`
+
+const  BottomDesc = styled(Desc)`
+    font-size: 16px;
+    font-weight: 400;
+    top: 2380px;
+    left: 50%;
+    transform: translateX(-50%);
+    line-height: 25px;
+    letter-spacing: 1px;
+`
+
+
 const MgmPage = () => {
     return (
         <Wrap>
@@ -119,6 +193,24 @@ const MgmPage = () => {
                 <EnglishTitle>
                     COLLECT<br/>A LOT OF LINE POINTS
                 </EnglishTitle>
+
+                <ContentDesc>
+                    <p>2024/10/1~2025/1/31</p>
+                    <p>成功邀請親友投保旅平險，達指定筆數</p>
+                </ContentDesc>
+                <LinePointsText src="./mgm/mgm-line-points-text.svg" />
+
+                <MgmContentOne src="./mgm/mgm-content1.svg" />
+                <MgmContentTwo src="./mgm/mgm-content2.svg" />
+                <MgmContentThree src="./mgm/mgm-content3.svg" />
+
+                <People src="./mgm/mgm-people-and-frame.svg" />
+                <ActionDesc><p>只要親友點選「你的推薦連結」成功投保</p></ActionDesc>
+                <ActionLinePoints src="./mgm/mgm-bottom-info-title.svg" />
+                <BottomDesc>
+                    <p>點擊上方按鈕後，註冊或登入取得邀請碼</p>
+                    <p>就能分享給親友，一起抽點數！</p>
+                </BottomDesc>
         
             </Container>
       </Wrap>
